519c1def8d879b0577949954ea9f0c3376a0cf57
diegomota2374 <diegomota2374@gmail.com>
2024-07-17 14:44:08 -0300

    primeiro commit, com a aplicação configurada

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -16,7 +16,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">OI sou o Diego!</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
